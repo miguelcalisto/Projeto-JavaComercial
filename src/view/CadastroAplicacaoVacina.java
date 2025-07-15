42c0fc126b088d3798b32fd23f551f932cd245ec
miguelcalisto <miguelcalistors@gmail.com>
2025-07-15 20:16:27 -0300

    main bug

--- src/view/CadastroAplicacaoVacina.java
+++ src/view/CadastroAplicacaoVacina.java
@@ -42,6 +42,10 @@ public class CadastroAplicacaoVacina extends javax.swing.JFrame {
     getAplicacaoVacina();
         
     }
+    //problema no main
+    public CadastroAplicacaoVacina() {
+    this(new Paciente()); }
+
     
     public void limpaCampos(){
         cmbProfissional.setSelectedIndex(-1);
@@ -476,12 +480,43 @@ public class CadastroAplicacaoVacina extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_txtLocalAplicActionPerformed
 
-        public static void main(String args[]) {
-    /* Set the Nimbus look and feel */
-    //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-    /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-     * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-     */
+//        public static void main(String args[]) {
+//    /* Set the Nimbus look and feel */
+//    //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//    /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//     * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//     */
+//    try {
+//        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//            if ("Nimbus".equals(info.getName())) {
+//                javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                break;
+//            }
+//        }
+//    } catch (ClassNotFoundException ex) {
+//        java.util.logging.Logger.getLogger(CadastroAplicacaoVacina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//    } catch (InstantiationException ex) {
+//        java.util.logging.Logger.getLogger(CadastroAplicacaoVacina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//    } catch (IllegalAccessException ex) {
+//        java.util.logging.Logger.getLogger(CadastroAplicacaoVacina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//    } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//        java.util.logging.Logger.getLogger(CadastroAplicacaoVacina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//    }
+//    //</editor-fold>
+//
+//    /* Create and display the form */
+//    java.awt.EventQueue.invokeLater(new Runnable() {
+//        public void run() {
+//            // probelma no main
+//            Paciente pacienteFake = new Paciente();
+//            pacienteFake.setId(1); 
+//            pacienteFake.setNome("Paciente de Teste");
+//            new CadastroAplicacaoVacina(pacienteFake).setVisible(true);
+//        }
+//    });
+//}
+
+    public static void main(String[] args) {
     try {
         for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
             if ("Nimbus".equals(info.getName())) {
@@ -489,30 +524,15 @@ public class CadastroAplicacaoVacina extends javax.swing.JFrame {
                 break;
             }
         }
-    } catch (ClassNotFoundException ex) {
-        java.util.logging.Logger.getLogger(CadastroAplicacaoVacina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (InstantiationException ex) {
-        java.util.logging.Logger.getLogger(CadastroAplicacaoVacina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (IllegalAccessException ex) {
-        java.util.logging.Logger.getLogger(CadastroAplicacaoVacina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+    } catch (Exception ex) {
         java.util.logging.Logger.getLogger(CadastroAplicacaoVacina.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
     }
-    //</editor-fold>
 
-    /* Create and display the form */
-    java.awt.EventQueue.invokeLater(new Runnable() {
-        public void run() {
-            // probelma no main
-            Paciente pacienteFake = new Paciente();
-            pacienteFake.setId(1); 
-            pacienteFake.setNome("Paciente de Teste");
-            new CadastroAplicacaoVacina(pacienteFake).setVisible(true);
-        }
+    java.awt.EventQueue.invokeLater(() -> {
+        new CadastroAplicacaoVacina().setVisible(true);
     });
 }
 
-    
     
     
     
